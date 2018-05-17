929e0686cd75d399c80e8acc266a4278b4e57d0a
Jhonatan Batista <jhonatanbds@outlook.com>
2018-05-17 00:06:13 -0300

    AM representation in progress

--- src/grafo/Aresta.java
+++ src/grafo/Aresta.java
@@ -1,4 +1,11 @@
 package grafo;
 
 public class Aresta {
+    public float getPeso() {
+        return 0;
+    }
+
+    public Integer getTargetVertex() {
+        return 1;
+    }
 }
--- src/grafo/GrafoBase.java
+++ src/grafo/GrafoBase.java
@@ -1,11 +1,12 @@
 package grafo;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
+import static java.lang.Character.LINE_SEPARATOR;
 
 public abstract class GrafoBase {
 
-    private List<Vertice> vertice = new ArrayList<Vertice>();
+    protected Map<Integer, Set<Aresta>> vertices;
     private List<Aresta> arestas = new ArrayList<Aresta>();
     private boolean pesos;
 
@@ -15,6 +16,50 @@ public abstract class GrafoBase {
 
     public GrafoBase() {
 
+    }
+
+    String graphRepresentation (GrafoBase graph, String type) {
+        if (type.equals("AM")) {
+            List<Integer> orderedVertexes = new ArrayList<Integer>(this.vertices.keySet());
+            Collections.sort(orderedVertexes);
+            int vertexesNumber = this.vertexNumber;
+            float adjacencyMatrix[][] = new float[vertexesNumber][vertexesNumber];
+            for(int i = 0; i < vertexesNumber; i++) {
+                Integer currentVertex = orderedVertexes.get(i);
+                Set<Aresta> connectedEdges = vertices.get(currentVertex);
+                for (Aresta edge : connectedEdges) {
+                    Integer targetVertex = edge.getTargetVertex();
+                    adjacencyMatrix[i][orderedVertexes.indexOf(targetVertex)] = edge.getPeso();
+                }
+            }
+            return getAdjacencyMatrixString(orderedVertexes, adjacencyMatrix);
+        } else if (type.equals("AL")) {
+            return "todo";
+        } else {
+            return "Tipo não definido";
+        }
+    }
+
+    protected String getAdjacencyMatrixString(List<Integer> orderedVertexes, float[][] adjacencyMatrix) {
+        int vertexesNumber = vertexNumber;
+        StringBuilder matrixSB = new StringBuilder("  ");
+
+        for (int i = 0; i < vertexesNumber; i++) {
+            matrixSB.append(orderedVertexes.get(i));
+            boolean shouldAddSpace = vertexesNumber - i > 1;
+            if (shouldAddSpace) matrixSB.append(" ");
+        }
+        matrixSB.append(LINE_SEPARATOR);
+        for(int i = 0; i < vertexesNumber; i++) {
+            StringBuilder line = new StringBuilder(orderedVertexes.get(i) + " ");
+            for(int j = 0; j < vertexesNumber; j++) {
+                line.append(Float.toString(adjacencyMatrix[i][j]));
+                boolean shouldAddSpace = vertexesNumber - j > 1;
+                if (shouldAddSpace) line.append(" ");
+            }
+            matrixSB.append(line).append(LINE_SEPARATOR);
+        }
+        return matrixSB.toString();
     }
 
     public float getMeanEdge() {
@@ -29,14 +74,14 @@ public abstract class GrafoBase {
         return vertexNumber;
     }
 
-    abstract String BFS(Grafo graph, Vertice vertex);
+    abstract String BFS(GrafoBase graph, Vertice vertex);
 
-    abstract String DFS(Grafo graph, Vertice vertex);
+    abstract String DFS(GrafoBase graph, Vertice vertex);
 
-    abstract boolean connected(Grafo graph);
+    abstract boolean connected(GrafoBase graph);
 
-    abstract String shortestPath(Grafo graph, Vertice head, Vertice tail);
+    abstract String shortestPath(GrafoBase graph, Vertice head, Vertice tail);
 
-    abstract String mst(Grafo graph);
+    abstract String mst(GrafoBase graph);
 
 }
