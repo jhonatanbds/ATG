6f862c099b9f3871d3b8bfee25b86a833781356b
AdsonSilva <adson.silva@ccc.ufcg.edu.br>
2018-05-17 23:53:26 -0300

    Read graph

--- src/grafo/GrafoPonderado.java
+++ src/grafo/GrafoPonderado.java
@@ -1,5 +1,6 @@
 package grafo;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -45,7 +46,24 @@ public class GrafoPonderado extends GrafoBase {
     }
 
     @Override
-    public void addAresta(String aresta) {
+    public void addAresta(String strAresta) {
+        String[] arestas = strAresta.split(" ");
+        Integer vertice1 = Integer.parseInt(arestas[0]);
+        Integer vertice2 = Integer.parseInt(arestas[1]);
+        Integer weight = Integer.parseInt(arestas[2]);
 
+
+        Aresta aresta = new Aresta(vertice1, vertice2, weight);
+        Aresta reverseAresta = new Aresta(vertice2, vertice1, weight);
+
+        if(!this.vertices.containsKey(vertice1)){
+            this.vertices.put(vertice1, new HashSet<Aresta>());
+        }
+        if(!this.vertices.containsKey(vertice2)){
+            this.vertices.put(vertice2, new HashSet<Aresta>());
+        }
+
+        this.vertices.get(vertice1).add(aresta);
+        this.vertices.get(vertice2).add(reverseAresta);
     }
 }
